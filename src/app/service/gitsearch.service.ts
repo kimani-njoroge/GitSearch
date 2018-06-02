bf1fce2d11aba99bf0ab81221c5480668de99608
kimani-njoroge <kimani064@gmail.com>
2018-06-02 15:18:25 +0300

    create constructor with property http and initialize username and outcome

--- src/app/service/gitsearch.service.ts
+++ src/app/service/gitsearch.service.ts
@@ -6,7 +6,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class GitsearchService {
 
+    private username: string;
+    private access_token: string = environment.access_token;
 
+    outcome: Object[];
+    outcome1: Object[];
+    outcome2: Object[];
+    outcome3: Object[];
 
-  constructor() { }
+  constructor(private http:HttpCLient) {
+    this.username = 'Kimani';
+    this.outcome = [];
+    this.outcome1 = [];
+    this.outcome2 = [];
+    this.outcome3 = [];
+  }
 }
